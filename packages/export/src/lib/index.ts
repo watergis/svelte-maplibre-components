c91142908d8e177c0a868ca29fdc34c5e99aa8dd
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-22 07:31:25 +0100

    merge MapExport component into both panel and control component

--- packages/export/src/lib/index.ts
+++ packages/export/src/lib/index.ts
@@ -1,8 +1,7 @@
-import { default as MapExport } from './MapExport.svelte';
 import { default as MapExportPanel } from './MapExportPanel.svelte';
 import { default as MapExportControl } from './MapExportControl.svelte';
 
-export { MapExport, MapExportPanel, MapExportControl };
+export { MapExportPanel, MapExportControl };
 
 export * from './utils/crosshair-manager';
 export * from './utils/printable-area-manager';

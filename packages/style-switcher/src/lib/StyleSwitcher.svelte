b5bac02235eac3218ed56626f44f1dbabfcb5f1f
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-12 11:00:56 +0000

    deleted bulma dependencies

--- packages/style-switcher/src/lib/StyleSwitcher.svelte
+++ packages/style-switcher/src/lib/StyleSwitcher.svelte
@@ -36,10 +36,50 @@
 	};
 </script>
 
-<div class="select is-link is-fullwidth mt-1">
-	<select bind:value={styleUrl}>
+<div class="style-container">
+	<select class="style-select" bind:value={styleUrl}>
 		{#each styles as style}
 			<option value={style.uri}>{style.title}</option>
 		{/each}
 	</select>
 </div>
+
+<style lang="scss">
+	.style-select {
+		cursor: pointer;
+		width: 100%;
+		height: 40px;
+		border-color: #485fc7;
+		font-size: 1em;
+		padding: 10px;
+	}
+
+	select {
+		-webkit-appearance: none;
+		appearance: none; /* デフォルトの矢印を非表示 */
+	}
+	select::-ms-expand {
+		display: none; /* デフォルトの矢印を非表示(IE用) */
+	}
+
+	.style-container::before {
+		border-bottom: 7px solid #485fc7;
+		border-left: 6px solid transparent;
+		border-right: 6px solid transparent;
+		content: '';
+		position: absolute;
+		right: 9px;
+		top: 12px;
+		width: 0;
+	}
+	.style-container::after {
+		border-left: 6px solid transparent;
+		border-right: 6px solid transparent;
+		border-top: 7px solid #485fc7;
+		content: '';
+		position: absolute;
+		right: 9px;
+		top: 23px;
+		width: 0;
+	}
+</style>

5caf63dfa1ecd1c551ad7d2031b6c233e9560ff2
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-13 23:37:52 +0000

    updated mdsvex

--- sites/svelte.water-gis.com/mdsvex.config.js
+++ sites/svelte.water-gis.com/mdsvex.config.js
@@ -1,23 +1,23 @@
-import remarkGithub from 'remark-github';
-import remarkAbbr from 'remark-abbr';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+// import remarkGithub from 'remark-github';
+// import remarkAbbr from 'remark-abbr';
+// import rehypeSlug from 'rehype-slug';
+// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default {
 	extensions: ['.svelte', '.md', '.svx'],
 	smartypants: {
 		dashes: 'oldschool'
-	},
-	remarkPlugins: [
-		[
-			remarkGithub,
-			{
-				// Use your own repository
-				repository: 'https://github.com/watergis/svelte-maplibre-components.git'
-			}
-		],
-		remarkAbbr
-	]
+	}
+	// remarkPlugins: [
+	// 	[
+	// 		remarkGithub,
+	// 		{
+	// 			// Use your own repository
+	// 			repository: 'https://github.com/watergis/svelte-maplibre-components.git'
+	// 		}
+	// 	],
+	// 	remarkAbbr
+	// ]
 	// rehypePlugins: [
 	// 	rehypeSlug,
 	// 	[

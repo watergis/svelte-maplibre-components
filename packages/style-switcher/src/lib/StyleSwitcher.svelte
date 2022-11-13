4d572ac2f3e70164e34593a17f8030cf80481c91
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-13 09:12:44 +0000

    Fix/style switcher css (#99)

--- packages/style-switcher/src/lib/StyleSwitcher.svelte
+++ packages/style-switcher/src/lib/StyleSwitcher.svelte
@@ -36,56 +36,19 @@
 	};
 </script>
 
-<div class="container">
-	<div class="style-container">
-		<select class="style-select" bind:value={styleUrl}>
-			{#each styles as style}
-				<option value={style.uri}>{style.title}</option>
-			{/each}
-		</select>
-	</div>
-</div>
+<select class="style-select" bind:value={styleUrl}>
+	{#each styles as style}
+		<option value={style.uri}>{style.title}</option>
+	{/each}
+</select>
 
 <style lang="scss">
-	.container {
-		position: relative;
-
-		.style-container::before {
-			border-bottom: 7px solid #485fc7;
-			border-left: 6px solid transparent;
-			border-right: 6px solid transparent;
-			content: '';
-			position: absolute;
-			right: 9px;
-			top: 12px;
-			width: 0;
-		}
-		.style-container::after {
-			border-left: 6px solid transparent;
-			border-right: 6px solid transparent;
-			border-top: 7px solid #485fc7;
-			content: '';
-			position: absolute;
-			right: 9px;
-			top: 23px;
-			width: 0;
-		}
-
-		.style-select {
-			cursor: pointer;
-			width: 100%;
-			height: 40px;
-			border-color: #485fc7;
-			font-size: 1em;
-			padding: 10px;
-		}
-
-		select {
-			-webkit-appearance: none;
-			appearance: none; /* デフォルトの矢印を非表示 */
-		}
-		select::-ms-expand {
-			display: none; /* デフォルトの矢印を非表示(IE用) */
-		}
+	.style-select {
+		cursor: pointer;
+		width: 100%;
+		height: 40px;
+		border-color: #485fc7;
+		font-size: 1em;
+		padding: 10px;
 	}
 </style>

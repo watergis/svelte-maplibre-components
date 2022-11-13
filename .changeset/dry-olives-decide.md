9e0e5c2c442f2a7b8909d83560d1756967aaa5d8
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-13 15:05:49 +0000

    fixed styleswitcher's css layout

---
+++ .changeset/dry-olives-decide.md
@@ -0,0 +1,5 @@
+---
+"@watergis/svelte-maplibre-style-switcher": patch
+---
+
+fixed styleswitcher's css layout
--- packages/style-switcher/src/lib/StyleSwitcher.svelte
+++ packages/style-switcher/src/lib/StyleSwitcher.svelte
@@ -47,8 +47,34 @@
 		cursor: pointer;
 		width: 100%;
 		height: 40px;
+		border-radius: 4px;
 		border-color: #485fc7;
+		box-sizing: border-box;
 		font-size: 1em;
 		padding: 10px;
+	}
+
+	select {
+		width: 100%;
+		border-radius: 4px;
+		border-color: #485fc7;
+		box-sizing: border-box;
+		background: transparent;
+		-webkit-appearance: none;
+		cursor: pointer;
+		background: #fff
+			url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2211%22%20height%3D%2211%22%20viewBox%3D%220%200%2011%2011%22%3E%3Cpath%20d%3D%22M4.33%208.5L0%201L8.66%201z%22%20fill%3D%22%23666%22%2F%3E%3C%2Fsvg%3E')
+			right 10px center no-repeat;
+		padding: 12px 35px 12px 11px;
+		color: #000;
+
+		/* Firefox hide arrow */
+		-moz-appearance: none;
+		text-indent: 0.01px;
+		text-overflow: '';
+	}
+	/* IE10 hide arrow */
+	select::-ms-expand {
+		display: none;
 	}
 </style>

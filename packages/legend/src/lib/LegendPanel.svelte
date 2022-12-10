95bd10f039cfbeaa472cda7f8970f5856c78a1ca
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-10 15:08:01 +0000

    prevent touchevent when feature is disabled

--- packages/legend/src/lib/LegendPanel.svelte
+++ packages/legend/src/lib/LegendPanel.svelte
@@ -92,6 +92,11 @@
 	};
 
 	const drop = (event: any, target: number, layer?: LayerSpecification) => {
+		if (!enableLayerOrder) {
+			event.preventDefault();
+			return;
+		}
+
 		event.dataTransfer.dropEffect = 'move';
 		const start = parseInt(event.dataTransfer.getData('text/plain'));
 		const newTracklist = allLayers;
@@ -117,6 +122,10 @@
 	};
 
 	const dragstart = (event: any, i: number) => {
+		if (!enableLayerOrder) {
+			event.preventDefault();
+			return;
+		}
 		event.dataTransfer.effectAllowed = 'move';
 		event.dataTransfer.dropEffect = 'move';
 		const start = i;
@@ -124,6 +133,10 @@
 	};
 
 	const dragover = (event: any) => {
+		if (!enableLayerOrder) {
+			event.preventDefault();
+			return;
+		}
 		event.preventDefault();
 		event.dataTransfer.dropEffect = 'move';
 	};
@@ -174,8 +187,14 @@
 										on:dragstart={(event) => dragstart(event, index)}
 										on:drop|preventDefault={(event) => drop(event, index, layer)}
 										on:dragover={(event) => dragover(event)}
-										on:dragenter={() => (hovering = index)}
-										class:is-active={hovering === index}
+										on:dragenter={(event) => {
+											if (!enableLayerOrder) {
+												event.preventDefault();
+												return;
+											}
+											hovering = index;
+										}}
+										class:is-active={enableLayerOrder ? hovering === index : false}
 									>
 										<li class="legend-panel-block">
 											<Layer
@@ -198,8 +217,14 @@
 									on:dragstart={(event) => dragstart(event, index)}
 									on:drop|preventDefault={(event) => drop(event, index, layer)}
 									on:dragover={(event) => dragover(event)}
-									on:dragenter={() => (hovering = index)}
-									class:is-active={hovering === index}
+									on:dragenter={(event) => {
+										if (!enableLayerOrder) {
+											event.preventDefault();
+											return;
+										}
+										hovering = index;
+									}}
+									class:is-active={enableLayerOrder ? hovering === index : false}
 								>
 									<li class="legend-panel-block">
 										<Layer
@@ -224,8 +249,14 @@
 								on:dragstart={(event) => dragstart(event, index)}
 								on:drop|preventDefault={(event) => drop(event, index, layer)}
 								on:dragover={(event) => dragover(event)}
-								on:dragenter={() => (hovering = index)}
-								class:is-active={hovering === index}
+								on:dragenter={(event) => {
+									if (!enableLayerOrder) {
+										event.preventDefault();
+										return;
+									}
+									hovering = index;
+								}}
+								class:is-active={enableLayerOrder ? hovering === index : false}
 							>
 								<li class="legend-panel-block">
 									<Layer
@@ -248,8 +279,14 @@
 							on:dragstart={(event) => dragstart(event, index)}
 							on:drop|preventDefault={(event) => drop(event, index, layer)}
 							on:dragover={(event) => dragover(event)}
-							on:dragenter={() => (hovering = index)}
-							class:is-active={hovering === index}
+							on:dragenter={(event) => {
+								if (!enableLayerOrder) {
+									event.preventDefault();
+									return;
+								}
+								hovering = index;
+							}}
+							class:is-active={enableLayerOrder ? hovering === index : false}
 						>
 							<li class="legend-panel-block">
 								<Layer
@@ -273,8 +310,14 @@
 						draggable={false}
 						on:drop|preventDefault={(event) => drop(event, getLastVisibleIndex())}
 						on:dragover={(event) => dragover(event)}
-						on:dragenter={() => (hovering = getLastVisibleIndex())}
-						class:is-active={hovering === getLastVisibleIndex()}
+						on:dragenter={(event) => {
+							if (!enableLayerOrder) {
+								event.preventDefault();
+								return;
+							}
+							hovering = getLastVisibleIndex();
+						}}
+						class:is-active={enableLayerOrder ? hovering === getLastVisibleIndex() : false}
 					>
 						{#if isShowLastDropArea}
 							<div class="last-drop-area">Drag to the last</div>

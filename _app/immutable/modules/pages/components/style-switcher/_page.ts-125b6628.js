005e9dfc04bdc5c3b7cca155440373be7a27eb10
watergis+githubci <watergis+githubci@users.noreply.github.com>
2022-12-10 18:22:01 +0000

    Updates

--- _app/immutable/modules/pages/components/style-switcher/_page.ts-125b6628.js
+++ _app/immutable/modules/pages/components/style-switcher/_page.ts-125b6628.js
@@ -1,1 +1,1 @@
-import{p as e,s as p}from"../../../../chunks/_page-bf2b9fa2.js";export{e as prerender,p as ssr};
+import{p as e,s as p}from"../../../../chunks/_page-2ee1ec03.js";export{e as prerender,p as ssr};

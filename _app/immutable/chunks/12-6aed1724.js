564d0e5d6fc57982af992cf56f509d19ec3442c7
watergis+githubci <watergis+githubci@users.noreply.github.com>
2022-12-15 10:56:10 +0000

    Updates

--- _app/immutable/chunks/12-6aed1724.js
+++ _app/immutable/chunks/12-6aed1724.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-0a1a7ddc.js";import{default as t}from"../components/pages/components/style-switcher/_page.md-dc777f70.js";export{t as component,r as universal};
+import{_ as r}from"./_page-bf2b9fa2.js";import{default as t}from"../components/pages/components/style-switcher/_page.md-dc777f70.js";export{t as component,r as universal};
--- _app/immutable/chunks/9-779826c1.js
+++ _app/immutable/chunks/9-779826c1.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-f58a9c48.js";import{default as t}from"../components/pages/components/menu/_page.md-e6269d6a.js";export{t as component,r as universal};
+import{_ as r}from"./_page-c39a56e1.js";import{default as t}from"../components/pages/components/menu/_page.md-e6269d6a.js";export{t as component,r as universal};

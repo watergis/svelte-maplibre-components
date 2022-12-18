f0968ae7c4e1c4ea0351607dce385540a5fb39c4
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-18 18:56:01 +0000

    removed esbuild from site

--- sites/svelte.water-gis.com/vite.config.ts
+++ sites/svelte.water-gis.com/vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
@@ -8,20 +7,6 @@ const config: UserConfig = {
 		fs: {
 			// Allow serving files from one level up to the project root
 			allow: ['../..']
-		}
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			// Node.js global to browser globalThis
-			define: {
-				global: 'globalThis'
-			},
-			// Enable esbuild polyfill plugins
-			plugins: [
-				NodeGlobalsPolyfillPlugin({
-					buffer: true
-				})
-			]
 		}
 	}
 };

796aabd46901ead2099752eedd42a42588a7b101
watergis+githubci <watergis+githubci@users.noreply.github.com>
2022-12-22 11:02:09 +0000

    Updates

--- _app/immutable/chunks/4-52388759.js
+++ _app/immutable/chunks/4-52388759.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-09cfbf65.js";import{default as t}from"../components/pages/components/center-icon/_page.md-d0d6cbc3.js";export{t as component,r as universal};
+import{_ as r}from"./_page-475d2f8d.js";import{default as t}from"../components/pages/components/center-icon/_page.md-d0d6cbc3.js";export{t as component,r as universal};
--- _app/immutable/chunks/7-1f1feb2b.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-475d2f8d.js";import{default as t}from"../components/pages/components/legend/_page.md-af6956a5.js";export{t as component,r as universal};

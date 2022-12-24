914cda65e794d4eb77fb39d7126024e493fff12d
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-24 10:08:17 +0000

    delete unused import

--- packages/legend/src/lib/Legend.svelte
+++ packages/legend/src/lib/Legend.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import type { LayerSpecification, Map } from 'maplibre-gl';
-	import chroma from 'chroma-js';
-
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	import LegendSymbol from '@watergis/legend-symbol';

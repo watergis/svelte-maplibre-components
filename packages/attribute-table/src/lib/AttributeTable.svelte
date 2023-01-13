936cea1a501381e884823bd2c462bf55268837cd
Jin Igarashi <igarashi.jin@water-gis.com>
2023-01-13 12:34:08 +0000

    added power table

--- packages/attribute-table/src/lib/AttributeTable.svelte
+++ packages/attribute-table/src/lib/AttributeTable.svelte
@@ -8,19 +8,23 @@
 		VectorSourceSpecification
 	} from 'maplibre-gl';
 	import { distinct } from './util/distinct';
+	import PowerTable, { type Data, type Instructs } from './powerTable/PowerTable.svelte';
 
 	export let map: Map;
 	export let sourceIds: string[];
 	export let height: number;
 	let controlHeader: number;
-	let tableHeight: number;
-	$: tableBodyHeight = height - controlHeader - tableHeight;
+	$: tableBodyHeight = height - controlHeader;
+
+	$: {
+		console.log(tableBodyHeight, height, controlHeader);
+	}
 
 	let layers: string[];
 	let selectedLayerId: string;
 
-	let rows: { [key: string]: string }[] = [];
-	let columns: { [key: string]: string }[] = [];
+	let ptData: Data[] = [];
+	let ptInstructs: Instructs[] = [];
 
 	$: if (map) {
 		map.on('sourcedata', (e) => {
@@ -70,15 +74,15 @@
 		if (!selectedLayerId) return;
 		if (!map) return;
 
-		rows = [];
-		columns = [];
+		ptData = [];
+		ptInstructs = [];
 		const features = map.queryRenderedFeatures(undefined, { layers: [selectedLayerId] });
-		rows = features.map((f) => f.properties);
+		ptData = features.map((f) => f.properties);
 
-		if (rows.length > 0) {
-			columns = Object.keys(rows[0]).map((key) => {
+		if (ptData.length > 0) {
+			ptInstructs = Object.keys(ptData[0]).map((key) => {
 				return {
-					name: key,
+					key: key,
 					title: key
 				};
 			});
@@ -95,33 +99,16 @@
 			{/each}
 		</select>
 	{/if}
-	<p class="subtitle is-6 p-0 m-0 pl-2">{rows.length} feature{rows.length > 1 ? 's' : ''} found.</p>
+	<p class="subtitle is-6 p-0 m-0 pl-2">
+		{ptData.length} feature{ptData.length > 1 ? 's' : ''} found.
+	</p>
 </div>
 
-<table class="attribute-table table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-	<thead bind:clientHeight={tableHeight}>
-		<tr>
-			{#each columns as col}
-				<th><p class="nowrap">{col.title}</p></th>
-			{/each}
-		</tr>
-	</thead>
-	<tbody style="height:{tableBodyHeight}px;">
-		{#if rows.length === 0}
-			<div class="notification is-info is-light">
-				No features found in current map area. Please try to move map.
-			</div>
-		{:else}
-			{#each rows as row}
-				<tr>
-					{#each columns as col}
-						<td><p class="text-wrap">{row[col.name]}</p></td>
-					{/each}
-				</tr>
-			{/each}
-		{/if}
-	</tbody>
-</table>
+<div class="attribute-table" style="height:{tableBodyHeight}px;">
+	<div class="MuonW PowerTable">
+		<PowerTable bind:ptData bind:ptInstructs />
+	</div>
+</div>
 
 <style lang="scss">
 	@use 'bulma/css/bulma.css';
@@ -131,28 +118,7 @@
 		align-items: center;
 	}
 
-	table tbody {
-		display: block;
-		max-height: 300px;
-		overflow-y: scroll;
-	}
-
-	table thead,
-	table tbody tr {
-		display: table;
-		width: 100%;
-		table-layout: fixed;
-	}
-
-	.nowrap {
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-
-	.text-wrap {
-		white-space: break-spaces;
-		overflow: hidden;
-		text-overflow: unset;
+	.attribute-table {
+		overflow-y: auto;
 	}
 </style>

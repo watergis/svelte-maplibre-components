4053f06acdf2a388d370027b10353bce6f3532cd
Jin Igarashi <igarashi.jin@water-gis.com>
2023-02-19 13:53:43 +0000

    improved heatmap legend

--- packages/legend/src/lib/Legend.svelte
+++ packages/legend/src/lib/Legend.svelte
@@ -5,6 +5,7 @@
 	import LegendSymbol from '@watergis/legend-symbol';
 	import type SpriteLoader from './sprite';
 	import { getColorFromExpression } from './util/getColorFromExpression';
+	import chroma from 'chroma-js';
 
 	export let map: Map;
 	export let layer: LayerSpecification;
@@ -45,10 +46,28 @@
 				const image = createSvgIcon(svg);
 				container.appendChild(image);
 			} else if (layer.type === 'heatmap') {
-				const svg =
-					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-160c-53 0-96-43-96-96s43-96 96-96s96 43 96 96s-43 96-96 96z"/></svg>';
-				const image = createSvgIcon(svg);
-				container.appendChild(image);
+				const color: string[] = map.getPaintProperty(layer.id, 'heatmap-color') as string[];
+				if (color && color[0] === 'interpolate') {
+					const colors: string[] = [];
+					for (let i = 4; i < color.length; i = i + 2) {
+						colors.push(color[i]);
+					}
+					const colormap = chroma
+						.scale(colors)
+						.mode('lrgb')
+						.padding([0.25, 0])
+						.domain([1, 100])
+						.colors(colors.length, 'rgba');
+					const cssStyle = `height: calc(1px * 24); width: calc(2px * 12); background: linear-gradient(90deg, ${colormap});`;
+					const divColor = document.createElement('div');
+					divColor.style.cssText = cssStyle;
+					container.appendChild(divColor);
+				} else {
+					const svg =
+						'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-160c-53 0-96-43-96-96s43-96 96-96s96 43 96 96s-43 96-96 96z"/></svg>';
+					const image = createSvgIcon(svg);
+					container.appendChild(image);
+				}
 			} else {
 				const color = map.getPaintProperty(layer.id, 'background-color');
 				const value = getColorFromExpression(color) ?? '#000000';

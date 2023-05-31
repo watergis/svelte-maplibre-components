3be5afacb9c49fa55614beeb9457be498f1e3694
Jin Igarashi <igarashi.jin@water-gis.com>
2023-05-31 10:59:17 +0900

    feat: migrated datatables lib to new repo's

--- packages/attribute-table/src/lib/AttributeTable.svelte
+++ packages/attribute-table/src/lib/AttributeTable.svelte
@@ -13,7 +13,7 @@
 		type LngLatLike
 	} from 'maplibre-gl';
 	import { distinct } from './util';
-	import { Datatable } from 'svelte-simple-datatables';
+	import { Datatable, DataHandler, Th, ThFilter } from '@vincjo/datatables';
 	import Fa from 'svelte-fa';
 	import {
 		faMagnifyingGlassPlus,
@@ -22,7 +22,7 @@
 	} from '@fortawesome/free-solid-svg-icons';
 
 	export let map: Map;
-	export let rowsPerPage = 25;
+	export let rowsPerPage = 50;
 	export let minZoom = 14;
 	let layers: string[];
 	let selectedSourceLayerId: string;
@@ -30,30 +30,10 @@
 	let containerHeight = 0;
 	let headerHeight = 0;
 
-	const settings = {
-		sortable: true,
-		pagination: true,
-		scrollY: true,
-		rowsPerPage: rowsPerPage,
-		columnFilter: true,
-		css: true,
-		labels: {
-			search: 'Search...', // search input placeholer
-			filter: 'Filter', // filter inputs placeholder
-			noRows: 'No entries to found',
-			info: 'Showing {start} to {end} of {rows} entries',
-			previous: 'Previous',
-			next: 'Next'
-		},
-		blocks: {
-			searchInput: true,
-			paginationButtons: true,
-			paginationRowCount: true
-		}
-	};
-
 	let features: MapGeoJSONFeature[] = [];
 	let data: { [key: string]: string }[] = [];
+
+	const handler = new DataHandler(data, { rowsPerPage });
 	let rows = writable<{ [key: string]: string }[]>([]);
 	let columns: { id: string; label: string }[] = [];
 
@@ -125,7 +105,25 @@
 		const ids = getLayerIdsBySourceLayer(selectedSourceLayerId);
 		if (ids.length > 0) {
 			features = map.queryRenderedFeatures(undefined, { layers: ids });
-			data = features.map((f) => f.properties);
+			data = [];
+			features.forEach((f) => {
+				const prop = f.properties;
+
+				const exists = data.find((d) => {
+					let allmatched = true;
+					Object.keys(d).forEach((key) => {
+						if (!(prop[key] && d[key] === prop[key])) {
+							allmatched = false;
+						}
+					});
+					if (allmatched) {
+						return d;
+					}
+				});
+				if (!exists) {
+					data.push(prop);
+				}
+			});
 
 			if (data.length > 0) {
 				let keys = data.flatMap((obj) => Object.keys(obj));
@@ -142,14 +140,12 @@
 						label: text
 					};
 				});
+
+				handler.setRows(data);
+				rows = handler.getRows();
 				return;
 			}
 		}
-
-		$rows = [];
-		features = [];
-		data = [];
-		columns = [];
 	};
 
 	const handleReload = () => {
@@ -228,7 +224,7 @@
 </script>
 
 <div class="attribute-table-container" bind:clientHeight={containerHeight}>
-	<aside class="header" bind:clientHeight={headerHeight}>
+	<header class="header" bind:clientHeight={headerHeight}>
 		<select class="layer-select" bind:value={selectedSourceLayerId}>
 			{#if layers && layers.length > 0}
 				{#each layers as id}
@@ -236,6 +232,7 @@
 				{/each}
 			{/if}
 		</select>
+
 		<button
 			class="reload-button"
 			on:click={handleReload}
@@ -243,32 +240,38 @@
 		>
 			<Fa icon={faRotate} size="nm" />
 		</button>
-	</aside>
+	</header>
 
-	<div class="attribute-table">
-		<section style="height: {containerHeight - headerHeight * 2}px">
-			{#if data.length === 0}
-				<p class="no-data">
-					No data to be shown in the selected layer and rendered area. Please select another layer
-					or move the map.
-				</p>
-			{:else}
-				{#key data}
-					<Datatable {settings} {data} bind:dataRows={rows}>
+	<div class="attribute-table" style="height: {containerHeight - headerHeight - 20}px">
+		{#if data.length === 0}
+			<p class="no-data">
+				No data to be shown in the selected layer and rendered area. Please select another layer or
+				move the map.
+			</p>
+		{:else}
+			{#key data}
+				<Datatable {handler}>
+					<table>
 						<thead>
 							<tr>
-								<th>Operation</th>
+								<Th {handler}>Operation</Th>
 								{#each columns as col}
-									<th data-key={col.id}>{col.label}</th>
+									<Th {handler} orderBy={col.id}>{col.label}</Th>
+								{/each}
+							</tr>
+							<tr>
+								<Th {handler} />
+								{#each columns as col}
+									<ThFilter {handler} filterBy={col.id} />
 								{/each}
 							</tr>
 						</thead>
 						<tbody>
-							{#if rows}
-								{#each $rows as row}
-									{@const feature = features.find((f) => f.properties === row)}
-									<tr>
-										<td>
+							{#each $rows as row}
+								{@const feature = features.find((f) => f.properties === row)}
+								<tr>
+									<td>
+										<div class="ope-col">
 											<button
 												class="operation-button"
 												on:click={() => {
@@ -285,22 +288,22 @@
 											>
 												<Fa icon={faUpDownLeftRight} size="lg" />
 											</button>
+										</div>
+									</td>
+									{#each columns as col}
+										<td>
+											{#if row[col.id]}
+												{row[col.id]}
+											{/if}
 										</td>
-										{#each columns as col}
-											<td>
-												{#if row[col.id]}
-													{row[col.id]}
-												{/if}
-											</td>
-										{/each}
-									</tr>
-								{/each}
-							{/if}
+									{/each}
+								</tr>
+							{/each}
 						</tbody>
-					</Datatable>
-				{/key}
-			{/if}
-		</section>
+					</table>
+				</Datatable>
+			{/key}
+		{/if}
 	</div>
 </div>
 
@@ -314,114 +317,125 @@
 		.header {
 			display: flex;
 			flex-direction: row;
-			margin-left: 32px;
-		}
+			gap: 5px;
+			margin-left: 16px;
 
-		.layer-select {
-			cursor: pointer;
-			width: 196px;
-			height: 26px;
-			margin-bottom: 0.5rem;
+			.layer-select {
+				cursor: pointer;
+				width: 196px;
+				height: 26px;
 
-			display: inline-flex;
-			align-items: center;
-			position: relative;
-		}
+				display: inline-flex;
+				align-items: center;
+				position: relative;
+			}
 
-		.layer-select::after {
-			position: absolute;
-			right: 15px;
-			width: 10px;
-			height: 7px;
-			background-color: #535353;
-			clip-path: polygon(0 0, 100% 0, 50% 100%);
-			content: '';
-			pointer-events: none;
-		}
+			.layer-select::after {
+				position: absolute;
+				right: 15px;
+				width: 10px;
+				height: 7px;
+				background-color: #535353;
+				clip-path: polygon(0 0, 100% 0, 50% 100%);
+				content: '';
+				pointer-events: none;
+			}
 
-		.layer-select select {
-			appearance: none;
-			min-width: 230px;
-			height: 2.8em;
-			padding: 0.4em calc(0.8em + 30px) 0.4em 0.8em;
-			border: 1px solid #cccccc;
-			border-radius: 3px;
-			background-color: #fff;
-			color: #333333;
-			font-size: 1em;
-			cursor: pointer;
+			.layer-select select {
+				appearance: none;
+				min-width: 230px;
+				height: 2.8em;
+				padding: 0.4em calc(0.8em + 30px) 0.4em 0.8em;
+				border: 1px solid #cccccc;
+				border-radius: 3px;
+				background-color: #fff;
+				color: #333333;
+				font-size: 1em;
+				cursor: pointer;
+			}
+
+			.reload-button {
+				cursor: pointer;
+				border: none;
+				width: 25px;
+				height: 25px;
+				border-radius: 5px;
+				background: #1c1c1c;
+				color: #ffffff;
+				align-items: center;
+				display: inline-flex;
+				justify-content: center;
+			}
+
+			.reload-button:disabled {
+				background: #a5a5a5;
+				cursor: default;
+			}
+
+			.reload-button:before {
+				line-height: 25px;
+				width: 25px;
+				text-align: center;
+				font-style: normal;
+				font-variant: normal;
+				text-rendering: auto;
+				-webkit-font-smoothing: antialiased;
+			}
 		}
 
 		.no-data {
 			margin-left: 32px;
 		}
 
-		.reload-button {
-			cursor: pointer;
-			border: none;
-			width: 27px;
-			height: 27px;
-			border-radius: 5px;
-			background: #1c1c1c;
-			color: #ffffff;
-			margin-left: 0.3rem;
-			align-items: center;
-			display: inline-flex;
-			justify-content: center;
-		}
-
-		.reload-button:disabled {
-			background: #a5a5a5;
-			cursor: default;
-		}
-
-		.reload-button:before {
-			line-height: 27px;
-			width: 27px;
-			text-align: center;
-			font-style: normal;
-			font-variant: normal;
-			text-rendering: auto;
-			-webkit-font-smoothing: antialiased;
-		}
-
 		.attribute-table {
-			aside {
+			header {
+				height: 48px;
+				padding: 0 16px;
 				display: flex;
 				justify-content: space-between;
-				padding: 8px 16px;
-				background: #fff;
-				border-radius: 8px;
-				margin: 8px 0;
-			}
-			th:first-child {
-				width: 72px;
-			}
-			td {
-				text-align: center;
-				padding: 4px 0;
+				align-items: center;
 			}
 
-			.operation-button {
-				cursor: pointer;
-				border: none;
-				width: 30px;
-				height: 30px;
-				border-radius: 25px;
-				background: #1c1c1c;
-				color: #fff;
-				align-items: center;
-				display: inline-flex;
-				justify-content: center;
+			thead {
+				background: #fff;
 			}
-			.operation-button:before {
-				line-height: 30px;
-				width: 30px;
-				text-align: center;
-				font-style: normal;
-				font-variant: normal;
-				text-rendering: auto;
-				-webkit-font-smoothing: antialiased;
+			tbody td {
+				border: 1px solid #f5f5f5;
+				padding: 4px 20px;
+			}
+			tbody tr {
+				transition: all, 0.2s;
+			}
+			tbody tr:hover {
+				background: #f5f5f5;
+			}
+
+			.ope-col {
+				display: flex;
+				flex-direction: row;
+				gap: 5px;
+
+				.operation-button {
+					cursor: pointer;
+					border: none;
+					width: 30px;
+					height: 30px;
+					border-radius: 25px;
+					background: #1c1c1c;
+					color: #fff;
+					align-items: center;
+					display: inline-flex;
+					justify-content: center;
+				}
+				.operation-button:before {
+					line-height: 30px;
+					width: 30px;
+					text-align: center;
+					font-style: normal;
+					font-variant: normal;
+					text-rendering: auto;
+					-webkit-font-smoothing: antialiased;
+				}
 			}
 		}
 	}

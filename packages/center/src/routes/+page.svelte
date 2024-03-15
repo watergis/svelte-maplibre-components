80e84331f86b7428287b9b68b6ed613a1ab76149
Jin Igarashi <igarashi.jin@water-gis.com>
2024-03-15 08:22:06 +0000

    chore: migrated to sveltekit v2 for center

--- packages/center/src/routes/+page.svelte
+++ packages/center/src/routes/+page.svelte
@@ -1,9 +1,42 @@
 <script lang="ts">
-	import Map from '$example/Map.svelte';
+	import CenterIconManager from '$lib';
+	import { addProtocol, Map } from 'maplibre-gl';
+	import { Protocol } from 'pmtiles';
+	import { onMount } from 'svelte';
+
+	let mapContainer: HTMLDivElement;
+	let centerIconManager: CenterIconManager;
+
+	let map: Map;
+
+	onMount(async () => {
+		const protocol = new Protocol();
+		addProtocol('pmtiles', protocol.tile);
+		map = new Map({
+			container: mapContainer,
+			style: 'https://narwassco.github.io/mapbox-stylefiles/unvt/style.json'
+		});
+
+		centerIconManager = new CenterIconManager(map);
+		centerIconManager.create();
+	});
 </script>
 
 <sveltekit:head>
 	<title>Svelte maplibre center icon example</title>
 </sveltekit:head>
 
-<Map />
+<div class="map" bind:this={mapContainer} />
+
+<style>
+	@import 'maplibre-gl/dist/maplibre-gl.css';
+
+	.map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 1;
+	}
+</style>
